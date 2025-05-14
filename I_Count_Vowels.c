de2cb054bd70dfbf717b4747466c7df586c233d9
Mmsshakil <softhandshakil@gmail.com>
2025-05-14 22:47:21 +0600

    Count Vowels by recoursion

--- I_Count_Vowels.c
+++ I_Count_Vowels.c
@@ -4,7 +4,18 @@ int rec(char s[], int i)
 {
     if (s[i] == '\0')
     {
-        /* code */
+        return 0;
+    }
+
+    int count = rec(s, i + 1);
+
+    if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+    {
+        return count + 1;
+    }
+    else
+    {
+        return count;
     }
 }
 
@@ -12,6 +23,7 @@ int main()
 {
     char s[201];
     fgets(s, 201, stdin);
-    rec(s, 0);
+    int result = rec(s, 0);
+    printf("%d", result);
     return 0;
 }
